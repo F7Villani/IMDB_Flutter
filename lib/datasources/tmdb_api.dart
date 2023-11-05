2c34c7466edf191e0b9a9a79cf2b68220b67c0b2
Felipe Villani <felipevillani.2000@hotmail.com>
2023-11-05 19:19:11 -0300

    feat: chamando api

---
+++ lib/datasources/tmdb_api.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:imdb/models/movie_model.dart';
+import 'package:imdb/models/person_model.dart';
+import 'package:imdb/models/serie_model.dart';
+
+class TMDB {
+
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3/',
+      queryParameters: {
+        'api_key': '6676f3084800ce7b3e6812acb1fbface',
+        'language': 'pt-BR',
+      },
+    )
+  );
+
+  TMDB();
+
+  Future<List<MovieModel>> getTrendingMovies() async {
+    final response = await _dio.get('/trending/movie/day');
+    return response.data['results'].map<MovieModel>((movie) => MovieModel.fromMap(movie)).toList();
+  }
+
+  Future<List<SerieModel>> getTrendingSeries() async {
+    final response = await _dio.get('/trending/tv/day');
+    return response.data['results'].map<SerieModel>((serie) => SerieModel.fromMap(serie)).toList();
+  }
+
+  Future<List<PersonModel>> getTrendingPeople() async {
+    final response = await _dio.get('/trending/person/day');
+    return response.data['results'].map<PersonModel>((serie) => PersonModel.fromMap(serie)).toList();
+  }
+
+}
